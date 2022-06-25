45763035e8b405521853ad012d17c4297c648551
David Bankole Abel <ballack_davidabel@yahoo.com>
2022-06-24 21:44:33 -0400

    updated

--- app/src/main/java/com/db/myscoreapp/MainActivity.java
+++ app/src/main/java/com/db/myscoreapp/MainActivity.java
@@ -5,15 +5,21 @@ package com.db.myscoreapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    public TextView tv;
+     TextView tm1Score;
+    TextView tm2score;
+    TextView scoreBy;
     int  scoreTemA = 0;
     int  scoreTemB = 0;
+    SeekBar seekbar;
 
 
 
@@ -21,60 +27,76 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tm1Score = (TextView)findViewById(R.id.textView1);
+        tm2score = (TextView) findViewById(R.id.textView2);
+        scoreBy = (TextView) findViewById(R.id.scoreBylbl);
 
-        tv = (TextView)findViewById(R.id.scoreTemA);
+        Button tm1Add = (Button) findViewById(R.id.tm1Add);
+        Button tm2Add = (Button) findViewById(R.id.tm2Add);
+        Button tm1Sub = (Button) findViewById(R.id.tm1Sub);
+        Button tm2Sub = (Button) findViewById(R.id.tm2Sub);
 
-    }
+        tm1Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int result = addMethod(Integer.parseInt(tm1Score.getText().toString()), Integer.parseInt(scoreBy.getText().toString()));
+                tm1Score.setText(String.valueOf(result));
+            }
+        });
+
+        tm2Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int result = addMethod(Integer.parseInt(tm2score.getText().toString()), Integer.parseInt(scoreBy.getText().toString()));
+                tm2score.setText(String.valueOf(result));
+            }
+        });
+
+        tm1Sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int result = subtractMethod(Integer.parseInt(tm1Score.getText().toString()), Integer.parseInt(scoreBy.getText().toString()));
+                tm1Score.setText(String.valueOf(result));
+            }
+        });
+
+        tm2Sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int result = subtractMethod(Integer.parseInt(tm2score.getText().toString()), Integer.parseInt(scoreBy.getText().toString()));
+                tm2score.setText(String.valueOf(result));
+            }
+        });
 
 
-    public void onRadioButtonClicked(View view) {
-        boolean checked = ((RadioButton) view).isChecked();
 
-        switch (view.getId()) {
-            case R.id.radioButton1TemA:
-                if (checked) {
-                    scoreTemA += 1;
-                }
-                break;
+        seekbar = (SeekBar)findViewById(R.id.seekBar);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
 
-            case R.id.radioButton2TemA:
-                if (checked)
-                    scoreTemA += 1;
-                break;
-
-            case R.id.radioButton3TemA:
-                if (checked)
-                    scoreTemA += 3;
-                break;
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            scoreBy.setText(String.valueOf(i));
+            System.out.println(i);
         }
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {}
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {}
+    });
 
-        switch (view.getId()) {
-            case R.id.radioButton1TemB:
-                if (checked) {
-                    scoreTemA += 1;
-                }
-                break;
+}
 
-            case R.id.radioButton2TemB:
-                if(checked)
-                    scoreTemA +=1;
-                break;
+    private int subtractMethod(int oldNumber, int newNumber) {
+        int result = oldNumber - newNumber;
+        return result;
 
-            case R.id.radioButton3TemB:
-                if(checked)
-                    scoreTemA +=3;
-                break;
-        }
-        updateScore(scoreTemA);
-        updateScore(scoreTemB);
     }
-    public void updateScore(int scoreTemA) {
-        tv.setText(" " + scoreTemA);
-    }
-    //Reset button code
-    public void resetButton(View view){
-        scoreTemA = 0;
-        scoreTemB = 0;
-
+    private int addMethod(int oldNumber, int newNumber){
+        int result = oldNumber + newNumber;
+        return result;
     }
 }
