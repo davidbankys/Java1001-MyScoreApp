0120415e8868b5c0b16d5d0d1f60e0bdaa03f100
David Bankole Abel <ballack_davidabel@yahoo.com>
2022-08-19 21:22:28 -0400

    main fragment created

---
+++ app/src/main/java/com/db/myscoreapp/Fragments.java
@@ -0,0 +1,14 @@
+package com.db.myscoreapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Fragments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
